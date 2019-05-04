2786683a35136f49c2495e4884f9141044312a05
Mitchell Bregman <mitchell_bregman@gallup.com>
2019-05-04 02:16:58 -0400

    added homepage, twilio sendmessage class for BizBackup (bb)

--- app.py
+++ app.py
@@ -1,25 +1,44 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, render_template, redirect
 from twilio.twiml.messaging_response import MessagingResponse
+
+from bbtwilio.SendMessage import *
+
 
 app = Flask(__name__)
 
-@app.route("/sms", methods=['GET', 'POST'])
+
+def send_message():
+    """Send a text message"""
+
+    bbtwilio = SendMessage()
+    return bbtwilio.send('+0000000000', 'yoo')
+
+
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/sms', methods=['GET', 'POST'])
 def incoming_sms():
     """Send a dynamic reply to an incoming text message"""
     # Get the message the user sent our Twilio number
+    number = request.form.get('From', None)
     body = request.values.get('Body', None)
-    print(body)
+    print('{}: {}'.format(number, body))
 
     # Start our TwiML response
     resp = MessagingResponse()
 
     # Determine the right reply for this message
     if body == 'hello':
-        resp.message("Hi!")
+        resp.message('Hi!')
     elif body == 'bye':
-        resp.message("Goodbye")
+        resp.message('Goodbye')
 
     return str(resp)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
